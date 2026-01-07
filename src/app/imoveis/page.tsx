b16f3fe6885f02c2ba5b2b6efa09aa11523973e6
cristoffer4-arch <cristoffer4@gmail.com>
2026-01-07 18:16:45 +0000

    Fix syntax error in use client directive

--- src/app/imoveis/page.tsx
+++ src/app/imoveis/page.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+'use client';;
 import { useEffect, useState } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
